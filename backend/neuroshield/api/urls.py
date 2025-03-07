23118035509f176148f7d6f4c17fc3534e2552d6
eugene-bosca <eugene.bosca@gmail.com>
2025-03-07 18:19:35 -0500

    edit urls

--- backend/neuroshield/api/urls.py
+++ backend/neuroshield/api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
 
@@ -24,4 +24,5 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
